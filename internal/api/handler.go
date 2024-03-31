a743c95796502cca5511fa33a67d81d290795470
Maurício Antunes <mauricio.abreua@gmail.com>
2024-03-31 17:34:11 -0300

    feat: Send source ID to API

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,7 +1,24 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Stream struct {
+	Source string `form:"source"`
+}
 
 func onReady(c *gin.Context) {
+	var stream Stream
+	if err := c.ShouldBindQuery(&stream); err != nil {
+		fmt.Println(err)
+		c.JSON(400, gin.H{"error": err})
+		return
+	}
+
+	fmt.Printf("Stream: %+v\n", stream)
+
 	c.JSON(200, gin.H{"status": "ok"})
 }
